2e0453590249526bd7fab1bc9983fc4e80234bf0
James Hrivnak <jameshrivnak4@gmail.com>
2021-12-11 20:04:37 -0700

    setup pm2

--- configs/pm2/ecosystem.config.js
+++ configs/pm2/ecosystem.config.js
@@ -4,20 +4,20 @@ const rootPath = path.resolve(__dirname, "../../");
 module.exports = {
   apps: [
     {
-      name: "minesweeper",
+      name: "strava-report-generator",
       script: path.join(rootPath, "src/server/index.js")
     }
   ],
   deploy: {
     production: {
       user: "ubuntu",
-      // host: " ec2-3-140-90-28.us-east-2.compute.amazonaws.com",
+      host: "ec2-3-135-119-109.us-east-2.compute.amazonaws.com",
       key: path.join(rootPath, "configs/aws/strava-report-gen.pem"),
-      // ref: "origin/master",
-      // repo: "git@github.com:jamesh48/Mini-Games.git",
-      // path: "/home/ubuntu/minesweeper",
-      // "post-deploy":
-        // "cd /home/ubuntu/minesweeper/source/beatminesweeper && npm install && npm run build && pm2 startOrRestart ./configs/pm2/ecosystem.config.js && pm2 save"
+      ref: "origin/main",
+      repo: "git@github.com:jamesh48/Strava-Report-Generator.git",
+      path: "/home/ubuntu/strava-report-generator",
+      "post-deploy":
+        "cd /home/ubuntu/strava-report-generator/source && npm install && npm run build && pm2 startOrRestart ./configs/pm2/ecosystem.config.js && pm2 save"
     }
   }
 };
