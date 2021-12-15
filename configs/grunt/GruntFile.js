5f496f08adac119b81b5a7d4eeff7b963cef3d91
James Hrivnak <jameshrivnak4@gmail.com>
2021-12-14 21:05:47 -0700

    One shot deploy script

--- configs/grunt/GruntFile.js
+++ configs/grunt/GruntFile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
       myConfig: require(path.join(
         rootPath,
         "configs/webpack/webpack.config.js"
-      )),
+      ))
     },
     gitadd: {
       task: {
@@ -26,18 +26,38 @@ module.exports = function (grunt) {
         }
       }
     },
-    gitpush: { your_target: {} }
+    gitpush: { your_target: {} },
+    cssmin: {
+      options: {
+        level: {
+          1: {
+            specialComments: 0
+          }
+        }
+      },
+      target: {
+        files: [
+          {
+            expand: true,
+            cwd: path.join(rootPath, "build/dist/public"),
+            src: ["*.css", "!*.min.css"],
+            dest: path.join(rootPath, "build/dist/public"),
+            ext: ".css"
+          }
+        ]
+      }
+    }
   });
 
   grunt.loadNpmTasks("grunt-webpack");
   grunt.loadNpmTasks("grunt-git");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
 
   grunt.registerTask("build", "webpack");
-
   // Deploy To AWS First
-  // grunt.registerTask("deploy", ["build", "uglify", "cssmin"]);
+  grunt.registerTask("deploy", ["build", /*"uglify",*/ "cssmin"]);
   // Push to Github
-  grunt.registerTask("deploy", ["build", "gitadd", "gitcommit", "gitpush"]);
+  grunt.registerTask("git", ["gitadd", "gitcommit", "gitpush"]);
 
   // Nuclear Option
   grunt.registerTask("deploy-all", ["deploy", "git"]);
